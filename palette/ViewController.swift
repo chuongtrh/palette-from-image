98343f8d802499963ef8c6255bb26c07b9bf7728
ChuongTran <chuongtrh@gmail.com>
2020-04-04 18:30:45 +0700

    Initial Commit

---
+++ palette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  palette
+//
+//  Created by Sam on 4/4/20.
+//  Copyright © 2020 Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
